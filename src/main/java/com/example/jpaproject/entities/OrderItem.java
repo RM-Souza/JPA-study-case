3963c543b7c3fc2d8b0f87f91afc19f6cca3a882
Rafael <rafael.sovza@gmail.com>
2022-06-25 13:21:02 -0300

    OrderItem, many-to-many association with extra attributes pt.1

---
+++ src/main/java/com/example/jpaproject/entities/OrderItem.java
@@ -0,0 +1,78 @@
+package com.example.jpaproject.entities;
+
+import com.example.jpaproject.entities.pk.OrderItemPK;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer qtd;
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer qtd, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.qtd = qtd;
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    public Integer getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(Integer qtd) {
+        this.qtd = qtd;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/example/jpaproject/entities/pk/OrderItemPK.java
@@ -0,0 +1,30 @@
+package com.example.jpaproject.entities.pk;
+
+import com.example.jpaproject.entities.Order;
+import com.example.jpaproject.entities.Product;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Embeddable
+public class OrderItemPK implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
