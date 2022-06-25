6072d75121a4d204cb1ab62be22db429ca5a005a
Rafael <rafael.sovza@gmail.com>
2022-06-24 21:20:15 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/jpaproject/entities/User.java
+++ src/main/java/com/example/jpaproject/entities/User.java
@@ -2,6 +2,7 @@ package com.example.jpaproject.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -10,10 +11,14 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+
+@Entity(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
