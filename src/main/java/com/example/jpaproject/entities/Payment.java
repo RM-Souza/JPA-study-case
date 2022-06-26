fa6946c1d43f2b118a62c0cb75c2b16013b0d89a
Rafael <rafael.sovza@gmail.com>
2022-06-25 21:24:20 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/example/jpaproject/entities/Payment.java
@@ -0,0 +1,30 @@
+package com.example.jpaproject.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+}
