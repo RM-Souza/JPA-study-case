89eaf7a1da72dfb18245548481176b1993cf58c1
Rafael <rafael.sovza@gmail.com>
2022-06-25 22:44:51 -0300

    Exceptions

--- src/main/java/com/example/jpaproject/services/UserService.java
+++ src/main/java/com/example/jpaproject/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.jpaproject.services;
 
 import com.example.jpaproject.entities.User;
 import com.example.jpaproject.repositories.UserRepository;
+import com.example.jpaproject.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
